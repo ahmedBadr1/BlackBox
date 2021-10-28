4a027e55fa80d03881770ecf6d56d0a84f95fd7b
feedbackwe4u <feedbackwe4u@gmail.com>
2021-10-28 16:37:29 +0200

    welcome message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->foreignIdFor(\App\Models\Business::class)->default(0);
             $table->foreignIdFor(\App\Models\Plan::class)->default(1);
-            $table->string('password');
+            $table->text('password');
             $table->boolean('active')->default(1);
             $table->dateTime('last_action_at')->nullable();
             $table->string('google_id')->nullable();
