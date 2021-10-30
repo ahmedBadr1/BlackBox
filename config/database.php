5885313a7a399df9c4012bbff87524c1e06a30a4
feedbackwe4u <feedbackwe4u@gmail.com>
2021-10-30 14:36:12 +0200

    schedule test

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'dump' => [
-                'dump_binary_path' => '/usr/local/bin/', // only the path, so without `mysqldump` or `pg_dump`
+              //  'dump_binary_path' => '/usr/local/bin/', // only the path, so without `mysqldump` or `pg_dump`
                 'use_single_transaction',
                // 'timeout' => 60 * 5, // 5 minute timeout
               //  'exclude_tables' => ['table1', 'table2'],
